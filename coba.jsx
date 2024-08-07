f76c51ac8f260dd30aa8ef8027f5cafdc5b1ae5f
Ali Akbar <aliakbar@users-MacBook-Pro.local>
2024-08-07 14:03:40 +0800

    ini adalah aplikasi baco

---
+++ coba.jsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export class coba extends Component {
+  render() {
+    return (
+      <View>
+        <Text>coba</Text>
+      </View>
+    );
+  }
+}
+
+export default coba;
